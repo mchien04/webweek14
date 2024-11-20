173916d4e8ad1e1ee5c1c1e2e9fa931d73201983
mchien04 <chienletranminh@gmail.com>
2024-11-20 15:19:53 +0700

    demo

---
+++ src/main/java/vn/iostar/Springtoolweek14Application.java
@@ -0,0 +1,13 @@
+package vn.iostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtoolweek14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtoolweek14Application.class, args);
+	}
+
+}
---
+++ src/test/java/vn/iostar/springtoolweek14/Springtoolweek14ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iostar.springtoolweek14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springtoolweek14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
